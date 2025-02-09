7dc46e21c2835cc5056d5436be38c4a1e3d50553
Camden Chin <ctc52@example.com>
2025-02-08 20:48:44 -0500

    vercel prep

--- HackDook/frontend/src/pages/Home.jsx
+++ HackDook/frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import axios from "axios";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 function Home() {
